5cb285e993e31af78b7d1f26b173cfa5adaaeaec
Cookseyd4905 <51975730+Cookseyd4905@users.noreply.github.com>
2019-06-18 19:32:49 -0400

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_DustinCooksey.py
@@ -0,0 +1,27 @@
+# This project shows the projected amount of profit
+# from a sales prediction
+# 06-14-2019
+# CTI-110 P2T1 - Sales Prediction
+# Dustin Cooksey
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of the total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+
+
+
+
+
+
+
+
+                          
+                              
+
+                          
